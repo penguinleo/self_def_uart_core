c81a1ea2a7c33b74eb5c15e0a439f60b78827ac2
Javen <penguinleo@163.com>
2019-12-02 02:02:05 +0800

    the shift register byte register fresh opperation is added but I need to consider the judgement of the byte register, just simplify the judgement with the shift register

--- ShiftRegister_Rx.v
+++ ShiftRegister_Rx.v
@@ -29,7 +29,8 @@ module ShiftRegister_Rx(
         input           Rx_i,
     // the interface with the FSM_Rx module
         input   [4:0]   State_i,
-        input           BitCounter_i,   // the index of the bit in the byte
+        input   [3:0]   BitCounter_i,   // the index of the bit in the byte
+        input           p_BigEnd_i,     // the Big end or the Little end
     // the output of the module
         output  [11:0]  Byte_o,     // the output of the shift register, including the data bits and the parity bit
     // the sychronization signal
@@ -39,15 +40,21 @@ module ShiftRegister_Rx(
         reg [2:0]   shift_reg_r;
         reg [15:0]  serial_reg_r;
         reg [3:0]   bit_width_cnt_r;   // this register was applied to measure the width of the rx signal 
+        reg [11:0]  byte_r;
     // wire definition 
         wire        falling_edge_rx_w;  // the falling edge of the rx port
         wire        rising_edge_rx_w;   // the rising edge of the rx port(reserved maybe no applied)
     // parameter definition
-        parameter   IDLE        = 5'b0_0001;   
-        parameter   STARTBIT    = 5'b0_0010;
-        parameter   DATABITS    = 5'b0_0100;
-        parameter   PARITYBIT   = 5'b0_1000;
-        parameter   STOPBIT     = 5'b1_0000;
+        // Receiving state machine definition  
+            parameter   IDLE        = 5'b0_0001;   
+            parameter   STARTBIT    = 5'b0_0010;
+            parameter   DATABITS    = 5'b0_0100;
+            parameter   PARITYBIT   = 5'b0_1000;
+            parameter   STOPBIT     = 5'b1_0000;
+            parameter   ACQSITION_POINT = 4'd7;
+        // Big end and littel end definition
+            parameter BIGEND    = 1'b1;
+            parameter LITTLEEND = 1'b0;
     // wire assign 
         assign falling_edge_rx_w    = shift_reg_r[2] & !shift_reg_r[1]; // falling edge of the rx
         assign rising_edge_rx_w     = !shift_reg_r[2]&  shift_reg_r[1];
@@ -91,5 +98,25 @@ module ShiftRegister_Rx(
                 bit_width_cnt_r <= bit_width_cnt_r;
             end
         end
-    // 
+    // byte register refresh
+        always @(posedge clk or negedge rst) begin
+            if (!rst) begin
+                byte_r <= 12'b0000_0000_0000;                
+            end
+            else if ((State_i == IDLE)) begin
+                byte_r <= 12'b0000_0000_0000;
+            end
+            else if ((State_i == STARTBIT) && (bit_width_cnt_r == ACQSITION_POINT)) begin
+                byte_r <= {byte_r[11:1],shift_reg_r[2]};
+            end
+            else if ((State_i == DATABITS) && (bit_width_cnt_r == ACQSITION_POINT) && (BitCounter_i == 4'd0)) begin  // the first data bit 
+                byte_r <= {byte_r[11:2],    shift_reg_r[2],     byte_r[0]};
+            end
+            else if ((State_i == DATABITS) && (bit_width_cnt_r == ACQSITION_POINT) && (BitCounter_i == 4'd1)) begin
+                byte_r <= {byte_r[11:3],    shift_reg_r[2],     byte_r[1:0]};
+            end
+            else if ((State_i == DATABITS) && (bit_width_cnt_r == ACQSITION_POINT) && (BitCounter_i == 4'd2)) begin
+                byte_r <= {byte_r[11:4],    shift_reg_r[2],     byte_r[2:0]};
+            end
+        end
 endmodule

d9e070cdf83b0ffe6d4c56be2c3153abca51ab45
Javen <penguinleo@163.com>
2020-05-20 19:37:54 +0800

    the control core was modified referred to the Zynq Uart core design which is the most similar with my requirements

--- CtrlCore.v
+++ CtrlCore.v
@@ -50,35 +50,49 @@ module CtrlCore(
     // write enable
         input           p_We_i,
     // input control register
-        input [7:0]     EnCodeCtrl_i,
-        input [7:0]     BaudRateGenHigh_i,
-        input [7:0]     BaudRateGenLow_i,
-        input [7:0]     BitCompensateMethod,
+        input [7:0]     ModeCtrl_i,             // mode control and encode control input
+        input [15:0]    BaudRateGen_i,          // Acqsignal divider control input(round down)
+        input [7:0]     BitCompensateMethod_i,  // Baudrate sigal divider
     // interupte control register
-        input [7:0]     InterrputCtrl_i,
-        input [7:0]     FifoInterrputNumHigh_i,
-        input [7:0]     FifoInterrputNumLow_i,
+        input [15:0]    InterrputEnable_i,      // Interrupt enbal bit
+        input [15:0]    InterruptMask_i,        // Interrupt mask bit     
+        input [15:0]    RxFIFOTriggerLevel_i,   // The Rx fifo trigger interrupt level
+        input [15:0]    TxFIFOTriggerLevel_i,   // The Tx fifo trigger interrupt level
     // output control register
+        output [7:0]    ModeCtrl_o,
         output [15:0]   BaudRateGen_o,
-        output [7:0]    BitCompensation_o,
-        output [3:0]    AcqNumPerBit_o,
+        output [7:0]    BitCompensateMethod_o,
+    // output interrupt state register
+        output [15:0]   InterrputEnable_o,
+        output [15:0]   InterruptMask_o,  
+        output [15:0]   InterruptState_o,
+        output [15:0]   UartState_o,
     // output protocol control register
+        output [3:0]    AcqNumPerBit_o,
         output          p_ParityEnable_o,
         output          p_BigEnd_o,
         output          ParityMethod_o
     );
-    // register 
+    // Register definition
         reg [15:0]  BaudRateGen_r;
         reg [7:0]   BitCompensation_r;
         reg         p_ParityEnable_r;
         reg         p_BigEnd_r;
         reg         ParityMethod_r;
         reg [3:0]   AcqNumPerBit_r;
+        reg [3:0]   UartMode_r;
+    // Logic definition
+
     // parameter
         // Default parameter
             parameter       DEFAULT_PERIOD      = 16'd20;
             parameter       DEFAULT_UP_TIME     = 4'd10;
             parameter       DEFAULT_DOWN_TIME   = 4'd5;
+        // Uart mode parameter
+            parameter       NORMAL_MODE         = 4'b0001;    // Normal mode, tx port sends data and rx port receives data
+            parameter       AUTO_ECHO_MODE      = 4'b0010;    // Automatic echo mode, rx port receives data and transfer to tx port
+            parameter       LOCAL_LOOPBACK_MODE = 4'b0100;    // Local loopback mode, rx port connected to the tx port directly would not send out
+            parameter       REMOTE_LOOPBACK_MODE= 4'b1000;    // Remote loopback mode, the input io and output io of uart was connected directly 
         // Parity Enable definition
             parameter ENABLE    = 1'b1;
             parameter DISABLE   = 1'b0;
@@ -95,30 +109,34 @@ module CtrlCore(
         assign p_BigEnd_o           = p_BigEnd_r;
         assign ParityMethod_o       = p_ParityEnable_r;
         assign AcqNumPerBit_o       = AcqNumPerBit_r;
-    always @(posedge clk or negedge rst) begin
-        if (!rst) begin
-            BaudRateGen_r       <= DEFAULT_PERIOD;
-            BitCompensation_r   <= {DEFAULT_UP_TIME,DEFAULT_DOWN_TIME};
-            p_ParityEnable_r    <= ENABLE;
-            p_BigEnd_r          <= LITTLEEND;
-            ParityMethod_r      <= ODD;
-            AcqNumPerBit_r      <= DEFAULT_UP_TIME + DEFAULT_DOWN_TIME;      
+    // register fresh 
+        always @(posedge clk or negedge rst) begin
+            if (!rst) begin
+                BaudRateGen_r       <= DEFAULT_PERIOD;
+                BitCompensation_r   <= {DEFAULT_UP_TIME,DEFAULT_DOWN_TIME};
+                p_ParityEnable_r    <= ENABLE;
+                p_BigEnd_r          <= LITTLEEND;
+                ParityMethod_r      <= ODD;
+                AcqNumPerBit_r      <= DEFAULT_UP_TIME + DEFAULT_DOWN_TIME;     
+                UartMode_r          <= NORMAL_MODE; 
+            end
+            else if (p_We_i == 1'b1) begin
+                BaudRateGen_r       <= BaudRateGen_i;
+                BitCompensation_r   <= BitCompensateMethod;
+                p_ParityEnable_r    <= ModeCtrl_i[6];
+                p_BigEnd_r          <= ModeCtrl_i[7];
+                ParityMethod_r      <= ModeCtrl_i[5];
+                UartMode_r          <= ModeCtrl_i[3:0];
+                AcqNumPerBit_r      <= BitCompensateMethod[7:4] + BitCompensateMethod[3:0];
+            end
+            else begin
+                BaudRateGen_r       <= BaudRateGen_r;
+                BitCompensation_r   <= BitCompensation_r;
+                p_ParityEnable_r    <= p_ParityEnable_r;
+                p_BigEnd_r          <= p_BigEnd_r;
+                ParityMethod_r      <= ParityMethod_r;
+                UartMode_r          <= UartMode_r;
+                AcqNumPerBit_r      <= AcqNumPerBit_r;
+            end
         end
-        else if (p_We_i == 1'b1) begin
-            BaudRateGen_r       <= {BaudRateGenHigh_i,BaudRateGenLow_i};
-            BitCompensation_r   <= BitCompensateMethod;
-            p_ParityEnable_r    <= EnCodeCtrl_i[6];
-            p_BigEnd_r          <= EnCodeCtrl_i[7];
-            ParityMethod_r      <= EnCodeCtrl_i[5];
-            AcqNumPerBit_r      <= BitCompensateMethod[7:4] + BitCompensateMethod[3:0];
-        end
-        else begin
-            BaudRateGen_r       <= BaudRateGen_r;
-            BitCompensation_r   <= BitCompensation_r;
-            p_ParityEnable_r    <= p_ParityEnable_r;
-            p_BigEnd_r          <= p_BigEnd_r;
-            ParityMethod_r      <= ParityMethod_r;
-            AcqNumPerBit_r      <= AcqNumPerBit_r;
-        end
-    end
 endmodule

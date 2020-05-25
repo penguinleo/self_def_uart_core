9b0485e8949d30502c8fbb5f5696d71f91f8418c
Javen <penguinleo@163.com>
2020-05-25 22:52:06 +0800

    address defined

--- CtrlCore.v
+++ CtrlCore.v
@@ -30,7 +30,7 @@
 //                                      Bit 3 ~ 0 are the number of round-down period in a bit time;
 //                                      The acquisite period control reg is the round-down period data.
 // Output Signal List:      
-//      1   |   BaudRateGen_o         :   The acquisite perid control register output. This period is the round-down
+//      1   |   BaudRateGen_o       :   The acquisite perid control register output. This period is the round-down
 //                                      period. This data is sent to the BaudGenerate module to generate the AcqSig
 //      2   |   BitCompensation_o   :   The compensate control register, which would help the BaudGenerate module
 //                                      reduce the bit width error less than a system clk;
@@ -43,7 +43,7 @@
 //      6   |   ParityMethod_o      :   The parity method select signal output for other uart submodule, 0-even,1-odd;
 // Register Map:
 //   Address | ConfigEn |IrqConfigEn| IrqLvlEn | Dir |         Name          |                                       Bit Definition                                          | 
-//   --------|----------|-----------|----------|-----|  -------------------  |     B7    |     B6    |     B5    |    B4     |     B3    |     B2    |    B1     |     B0    |
+//   --------|----------|-----------|----------|-----|-----------------------|     B7    |     B6    |     B5    |    B4     |     B3    |     B2    |    B1     |     B0    |
 //    3'b000 |     X    |     X     |     X    | R/W |      UartControl      | ConfigEn  |IrqConfigEn|  IrqLvlEn |   ClkSel  |   RxEn    |    TxEn   |   RxRst   |   TxRst   |
 //    3'b001 |     1    |     0     |     0    | R/W |       UartMode        |                   ModeSel                     |   EndSel  |   ParEn   |  ParSel   |  Reserved |  
 //    3'b010 |     1    |     0     |     0    | R/W |   BaudGeneratorHigh   |   High 8 bits of the Baudrate generator register, write access enabled when ConfigEn == 1     | 
@@ -53,12 +53,14 @@
 //    3'b010 |     1    |     1     |     0    |  W  |    InterrputEnable2   |   PARITY  |  FRAMING  |   OVER    |   TXFUL   |  TXEMPTY  |   RXFULL  |  RXEMPTY  |   RXOVR   | 
 //    3'b011 |     1    |     1     |     0    |  R  |    InterruptMask1     | Reserved  | Reserved  |   RBRK    |   TOVR    |   TNFUL   |   TTRIG   | Reserved  |   TOUT    |
 //    3'b100 |     1    |     1     |     0    |  R  |    InterruptMask2     |   PARITY  |  FRAMING  |   OVER    |   TXFUL   |  TXEMPTY  |   RXFULL  |  RXEMPTY  |   RXOVR   |
-//    3'b000 |     1    |     1     |     1    | R/W |    RxTrigLevelHigh    |     High 8 bits of the rx fifo trigger level       
-//    3'b001 |     1    |     1     |     1    | R/W |    RxTrigLevelLow     |     Low 8 bits of the rx fifo trigger level
-//    3'b010 |     1    |     1     |     1    | R/W |    TxTrigLevelHigh    |     High 8 bits of the tx fifo trigger level
-//    3'b011 |     1    |     1     |     1    | R/W |    TxTrigLevelLow     |     Low 8 bits of the tx fifo trigger level
+//    3'b001 |     1    |     1     |     1    | R/W |    RxTrigLevelHigh    |     High 8 bits of the rx fifo trigger level                                                         |      
+//    3'b010 |     1    |     1     |     1    | R/W |    RxTrigLevelLow     |     Low 8 bits of the rx fifo trigger level                                                   |
+//    3'b011 |     1    |     1     |     1    | R/W |    TxTrigLevelHigh    |     High 8 bits of the tx fifo trigger level                                                  |    
+//    3'b100 |     1    |     1     |     1    | R/W |    TxTrigLevelLow     |     Low 8 bits of the tx fifo trigger level                                                   |
 //    3'b001 |     0    |     0     |     0    | R/W |   InterruptStatus1    | Reserved  | Reserved  |   RBRK    |   TOVR    |   TNFUL   |   TTRIG   | Reserved  |   TOUT    |     
 //    3'b010 |     0    |     0     |     0    | R/W |   InterruptStatus2    |   PARITY  |  FRAMING  |   OVER    |   TXFUL   |  TXEMPTY  |   RXFULL  |  RXEMPTY  |   RXOVR   | 
+//    3'b011 |     0    |     0     |     0    |  R  | BytesNumberReceived1  |   High 8 bits of the bytes' number in receive fifo                                            |
+//    3'b100 |     0    |     0     |     0    |  R  | BytesNumberReceived2  |   Low 8 bits of the bytes' number in receive fifo                                             |
 //    3'b101 |     X    |     X     |     X    |  R  |      UartStatus1      | Reserved  |   TNFUL   |   TTRIG   | Reserved  |  TACTIVE  |  RACTIVE  | Reserved  | Reserved  |
 //    3'b110 |     X    |     X     |     X    |  R  |      UartStatus2      | Reserved  | Reserved  | Reserved  |   TXFUL   |  TXEMPTY  |   RXFULL  |  RXEMPTY  |   RXOVR   |
 //    3'b111 |     X    |     X     |     X    |  R  |      RxDataPort       |     8 bit receive data read port
@@ -69,7 +71,7 @@
 module CtrlCore(
     input   clk,
     input   rst,
-    // The address bus 
+    // The bus interface
         input  [2:0]    AddrBus_i,                  // the input address bus
         input           n_ChipSelect_i,             // the chip select signal
         input           n_rd_i,                     // operation direction control signal read direction, negative enable
@@ -119,21 +121,73 @@ module CtrlCore(
         reg [15:0]  InterruptMask_r1            /*synthesis syn_preserve = 1*/;  // R the interrupt enable signal controlled 
         reg [15:0]  InterruptState_r1           /*synthesis syn_preserve = 1*/;  // R/W the interrupt signal and clear control register 
         reg [15:0]  UartState_r1                /*synthesis syn_preserve = 1*/;  // R the uart state register
-    //
         reg [3:0]   BaudDivider_r1;
     // Logic definition
-
+        // page control signal definition
+            wire        ConfigEn_w;
+            wire        IrqConfigEn_w;
+            wire        IrqLvlEn_w;
+        // control signal definition
+            wire        ClkSel_w;
+            wire        RxEn_w;
+            wire        TxEn_w;
+            wire [3:0]  ModeSel_w;
+            wire        EndSel_w;
+            wire        ParEn_w;
+            wire        ParSel_w;
+        // Bus control signal logic
+            wire        ChipWriteAccess_w;      // The chip selected signal and write signal available together.
+            wire        ChipReadAccess_w;       // The chip selected signal and read signal available together.
+        // Register write access available
+            wire        UartControl_Write_Access_w;   
+            wire        UartMode_Write_Access_w;
+            wire        BaudGeneratorHigh_Write_Access_w;
+            wire        BaudGeneratorLow_Write_Access_w;
+            wire        BitCompensateMethod_Write_Access_w;
+            wire        InterrputEnable1_Write_Access_w;
+            wire        InterrputEnable2_Write_Access_w;
+            wire        InterruptMask1_Write_Access_w;
+            wire        InterruptMask2_Write_Access_w;
+            wire        RxTrigLevelHigh_Write_Access_w;
+            wire        RxTrigLevelLow_Write_Access_w;
+            wire        TxTrigLevelHigh_Write_Access_w;
+            wire        TxTrigLevelLow_Write_Access_w;
+            wire        InterruptStatus1_Write_Access_w;
+            wire        InterruptStatus2_Write_Access_w;
+            wire        BytesNumberReceived1_Write_Access_w;
+            wire        BytesNumberReceived2_Write_Access_w;
+            wire        UartStatus1_Write_Access_w;
+            wire        UartStatus2_Write_Access_w;
+            wire        RxDataPort_Write_Access_w;
+            wire        TxDataPort_Write_Access_w;         
     // parameter
         // Address definition
-            parameter       ADDR_UartControl            = 3'd0;     // W
-            parameter       ADDR_UartMode               = 3'd1;     // R/W
-            parameter       ADDR_BaudGenerator          = 3'd2;     // R/W
-            parameter       ADDR_BitCompensateMethod    = 3'd3;     // R/W
-            parameter       ADDR_InterrputEnable        = 3'd4;     // W, the write direction
-            parameter       ADDR_InterruptMask          = 3'd5;     // R/W, the read direction    
-            parameter       ADDR_UartState              = 3'd6;     // R, read only Uart state
-            parameter       ADDR_UartFIFOData           = 3'd7;     // R/W, R-rx fifo port, W-tx fifo port 
+            parameter   ADDR_UartControl                = 3'b000;
+            parameter   ADDR_UartMode                   = 3'b001;
+            parameter   ADDR_BaudGeneratorHigh          = 3'b010;
+            parameter   ADDR_BaudGeneratorLow           = 3'b011;
+            parameter   ADDR_BitCompensateMethod        = 3'b100;
+            parameter   ADDR_InterrputEnable1           = 3'b001;
+            parameter   ADDR_InterrputEnable2           = 3'b010;
+            parameter   ADDR_InterruptMask1             = 3'b011;
+            parameter   ADDR_InterruptMask2             = 3'b100;
+            parameter   ADDR_RxTrigLevelHigh            = 3'b001;
+            parameter   ADDR_RxTrigLevelLow             = 3'b010;
+            parameter   ADDR_TxTrigLevelHigh            = 3'b011;
+            parameter   ADDR_TxTrigLevelLow             = 3'b100;
+            parameter   ADDR_InterruptStatus1           = 3'b001;
+            parameter   ADDR_InterruptStatus2           = 3'b010;
+            parameter   ADDR_BytesNumberReceived1       = 3'b011;
+            parameter   ADDR_BytesNumberReceived2       = 3'b100;
+            parameter   ADDR_UartStatus1                = 3'b101;
+            parameter   ADDR_UartStatus2                = 3'b110;
+            parameter   ADDR_RxDataPort                 = 3'b111;
+            parameter   ADDR_TxDataPort                 = 3'b111;
         // Function definition
+            parameter   ON      = 1'b1;
+            parameter   OFF     = 1'b0;
+            parameter   N_ON    = 1'b0;
+            parameter   N_OFF   = 1'b1;
             //  UartControl bit 
                 // ConfigEN
                     parameter       UartControl_ConfigEn_ON     = 1'b1;   // In this state the cpu cound access the uart port configuration register
@@ -141,6 +195,9 @@ module CtrlCore(
                 // IrqConfigEn
                     parameter       UartControl_IrqConfigEn_ON  = 1'b1;
                     parameter       UartControl_IrqConfigEn_OFF = 1'b0;
+                // IrqLvlEn
+                    parameter       UartControl_IrqLvlEn_ON     = 1'b1;
+                    parameter       UartControl_IrqLvlEn_OFF    = 1'b0;
                 // ClkSel
                     parameter       UartControl_ClkSel_Time1    = 1'b0;
                     parameter       UartControl_ClkSel_Time8    = 1'b1;
@@ -152,37 +209,64 @@ module CtrlCore(
                     parameter       UartControl_RxEn_OFF        = 1'b0;
             // UartMode bit definition
                 // ModeSel  --UartMode Definition 
-                    parameter       NORMAL_MODE                 = 4'b0001;    // Normal mode, tx port sends data and rx port receives data
-                    parameter       AUTO_ECHO_MODE              = 4'b0010;    // Automatic echo mode, rx port receives data and transfer to tx port
-                    parameter       LOCAL_LOOPBACK_MODE         = 4'b0100;    // Local loopback mode, rx port connected to the tx port directly would not send out
-                    parameter       REMOTE_LOOPBACK_MODE        = 4'b1000;    // Remote loopback mode, the input io and output io of uart was connected directly         
+                    parameter       UartMode_NORMAL             = 4'b0001;    // Normal mode, tx port sends data and rx port receives data
+                    parameter       UartMode_AUTO_ECHO          = 4'b0010;    // Automatic echo mode, rx port receives data and transfer to tx port
+                    parameter       UartMode_LOCAL_LOOPBACK     = 4'b0100;    // Local loopback mode, rx port connected to the tx port directly would not send out
+                    parameter       UartMode_REMOTE_LOOPBACK    = 4'b1000;    // Remote loopback mode, the input io and output io of uart was connected directly  
+                // EndSel
+                    parameter       UartMode_BIGEND             = 1'b1;
+                    parameter       UartMode_LITTLEEND          = 1'b0;    
+                // ParEn
+                    parameter       UartMode_Parity_ENABLE      = 1'b1;
+                    parameter       UartMode_Parity_DISABLE     = 1'b0;
+                // ParSel
+                    parameter       UartMode_ParSel_EVEN        = 1'b0;
+                    parameter       UartMode_ParSel_ODD         = 1'b1;
         // Default parameter -- BaudRateGen & BitCompensateMethod
             parameter       DEFAULT_PERIOD      = 16'd20;
             parameter       DEFAULT_UP_TIME     = 4'd10;
             parameter       DEFAULT_DOWN_TIME   = 4'd5;
-        
-        // Parity Enable definition  -- UartMode Defintion 
-            parameter ENABLE    = 1'b1;
-            parameter DISABLE   = 1'b0;
-        // Big end and littel end definition --UartMode Defintion
-            parameter BIGEND    = 1'b1;
-            parameter LITTLEEND = 1'b0;
-        // parity method definition  --UartMode Defintion
-            parameter EVEN      = 1'b0;
-            parameter ODD       = 1'b1;
-    // assign
-        assign BaudRateGen_o        = BaudRateGen_r;
-        assign BitCompensation_o    = BitCompensation_r;
-        assign p_ParityEnable_o     = p_ParityEnable_r;
-        assign p_BigEnd_o           = p_BigEnd_r;
-        assign ParityMethod_o       = p_ParityEnable_r;
-        assign AcqNumPerBit_o       = AcqNumPerBit_r;
+    // Logic assign definition
+        // page control signal definition
+            assign ConfigEn_w       = UartControl_r1[7];
+            assign IrqConfigEn_w    = UartControl_r1[6];
+            assign IrqLvlEn_w       = UartControl_r1[5];
+        // control signal definition
+            assign ClkSel_w         = UartControl_r1[4];
+            assign RxEn_w           = UartControl_r1[3];
+            assign TxEn_w           = UartControl_r1[2];
+            assign ModeSel_w        = UartMode_r1[7:4];
+            assign EndSel_w         = UartMode_r1[3];
+            assign ParEn_w          = UartMode_r1[2];
+            assign ParSel_w         = UartMode_r1[1];
+        // Bus control signal logic 
+            assign ChipWriteAccess_w                    = (n_ChipSelect_i == 1'b0) && (n_we_i == 1'b0);\
+
+        // Register write access available
+            assign UartControl_Write_Access_w           = ChipWriteAccess_w && (AddrBus_i == ADDR_UartControl);  
+            assign UartMode_Write_Access_w              = ChipWriteAccess_w && (AddrBus_i == ADDR_UartMode              ) && (ConfigEn_w == ON ) && (IrqConfigEn_w == OFF) && (IrqLvlEn_w == OFF);   
+            assign BaudGeneratorHigh_Write_Access_w     = ChipWriteAccess_w && (AddrBus_i == ADDR_BaudGeneratorHigh     ) && (ConfigEn_w == ON ) && (IrqConfigEn_w == OFF) && (IrqLvlEn_w == OFF);
+            assign BaudGeneratorLow_Write_Access_w      = ChipWriteAccess_w && (AddrBus_i == ADDR_BaudGeneratorLow      ) && (ConfigEn_w == ON ) && (IrqConfigEn_w == OFF) && (IrqLvlEn_w == OFF);
+            assign BitCompensateMethod_Write_Access_w   = ChipWriteAccess_w && (AddrBus_i == ADDR_BitCompensateMethod   ) && (ConfigEn_w == ON ) && (IrqConfigEn_w == OFF) && (IrqLvlEn_w == OFF);
+            assign InterrputEnable1_Write_Access_w      = ChipWriteAccess_w && (AddrBus_i == ADDR_InterrputEnable1      ) && (ConfigEn_w == ON ) && (IrqConfigEn_w == ON ) && (IrqLvlEn_w == OFF);
+            assign InterrputEnable2_Write_Access_w      = ChipWriteAccess_w && (AddrBus_i == ADDR_InterrputEnable2      ) && (ConfigEn_w == ON ) && (IrqConfigEn_w == ON ) && (IrqLvlEn_w == OFF);
+            assign RxTrigLevelHigh_Write_Access_w       = ChipWriteAccess_w && (AddrBus_i == ADDR_RxTrigLevelHigh       ) && (ConfigEn_w == ON ) && (IrqConfigEn_w == ON ) && (IrqLvlEn_w == ON );
+            assign RxTrigLevelLow_Write_Access_w        = ChipWriteAccess_w && (AddrBus_i == ADDR_RxTrigLevelLow        ) && (ConfigEn_w == ON ) && (IrqConfigEn_w == ON ) && (IrqLvlEn_w == ON );
+            assign TxTrigLevelHigh_Write_Access_w       = ChipWriteAccess_w && (AddrBus_i == ADDR_TxTrigLevelHigh       ) && (ConfigEn_w == ON ) && (IrqConfigEn_w == ON ) && (IrqLvlEn_w == ON );
+            assign TxTrigLevelLow_Write_Access_w        = ChipWriteAccess_w && (AddrBus_i == ADDR_TxTrigLevelLow        ) && (ConfigEn_w == ON ) && (IrqConfigEn_w == ON ) && (IrqLvlEn_w == ON );
+            assign InterruptStatus1_Write_Access_w      = ChipWriteAccess_w && (AddrBus_i == ADDR_InterruptStatus1      ) && (ConfigEn_w == OFF) && (IrqConfigEn_w == OFF) && (IrqLvlEn_w == OFF);
+            assign InterruptStatus2_Write_Access_w      = ChipWriteAccess_w && (AddrBus_i == ADDR_InterruptStatus2      ) && (ConfigEn_w == OFF) && (IrqConfigEn_w == OFF) && (IrqLvlEn_w == OFF);
+            assign TxDataPort_Write_Access_w            = ChipWriteAccess_w && (AddrBus_i == ADDR_TxDataPort);  
     // UartControl register fresh 
         always @(posedge clk or negedge rst) begin
             if (!rst) begin
-                UartControl_r1 <=                 
+                UartControl_r1 <= { UartControl_ConfigEn_ON,    UartControl_IrqConfigEn_OFF, 
+                                    UartControl_IrqLvlEn_OFF,   UartControl_ClkSel_Time1,
+                                    UartControl_RxEn_OFF,       UartControl_TxEn_OFF,
+                                    2'b00
+                                    }                
             end
-            else if () begin
+            else if (UartControl_Write_Access_w == ON ) begin
                 
             end
         end
